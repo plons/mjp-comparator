fa0cd898c0417065086bd87a64075b90c6e8dadb
Peter Lodewyckx <plodewyckx@key.net>
2014-11-25 22:42:34 +0100

    call_cmake.sh: specify debug option so we can debug

--- call_cmake.sh
+++ call_cmake.sh
@@ -2,5 +2,8 @@
 
 mkdir build
 pushd build
-cmake  "-GEclipse CDT4 - Unix Makefiles" -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=true ../project; result=$?
+cmake  "-GEclipse CDT4 - Unix Makefiles" \
+   -DCMAKE_BUILD_TYPE=Debug \
+   -DCMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=true \
+   ../project; result=$?
 popd
