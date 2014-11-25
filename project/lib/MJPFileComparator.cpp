85e3e6f2242a38cd51a2b6953a936f652b80c7a5
Peter Lodewyckx <plodewyckx@key.net>
2014-11-25 22:01:51 +0100

    Added MJPFileComparator

---
+++ project/lib/MJPFileComparator.cpp
@@ -0,0 +1,87 @@
+#include "MJPFileComparator.h"
+
+using boost::filesystem::path;
+using std::vector;
+
+
+MJPFileComparator::MJPFileComparator(const path& newFoxBeleidFile, const path& newCustomFile) :
+		foxBeleidFile(newFoxBeleidFile, convertFoxBeleidMJPLine),
+		customFile(newCustomFile, convertCustomMJPLine)
+{
+	std::cout << "Comparing following files:\n";
+	std::cout << "\tFoxbeleid file: " << foxBeleidFile.getPath() << " containing " << foxBeleidFile.getAllEntries().size() << " entries." << std::endl;
+	std::cout << "\tCustom file: " << customFile.getPath() << " containing " << customFile.getAllEntries().size() << " entries." << std::endl;
+	std::cout << "========================================================" << std::endl;
+}
+
+vector<MJPEntry> MJPFileComparator::getEntriesMissingInFoxBeleid() const
+{
+	vector<MJPEntry> missingEntries;
+	for (auto& entry : customFile.getAllEntries())
+	{
+		if (!foxBeleidFile.containsKey(entry.getKey()))
+		{
+			missingEntries.push_back(entry);
+		}
+	}
+	return missingEntries;
+}
+
+static bool onlyFirstAmountIsNotZero(const vector<double>& amounts)
+{
+	assert(amounts.size() > 0);
+	for (uint i = 1; i < amounts.size(); ++i)
+	{
+		if (amounts[i] > 0) return false;
+	}
+	return true;
+}
+
+vector<MJPEntry> MJPFileComparator::getEntriesMissingInCustomFile() const
+{
+	vector<MJPEntry> missingEntries;
+	for (auto& entry : foxBeleidFile.getAllEntries())
+	{
+		if (!customFile.containsKey(entry.getKey()))
+		{
+
+			if (!onlyFirstAmountIsNotZero(entry.getAmounts()))
+			{
+				missingEntries.push_back(entry);
+			}
+		}
+	}
+	return missingEntries;
+}
+
+
+namespace std {
+
+static std::ostream& operator<<(std::ostream& ws, const std::vector<double>& amounts)
+{
+	std::copy(amounts.begin(), amounts.end(), std::ostream_iterator<double>(ws, " "));
+	return ws;
+}
+
+}  // namespace std
+
+
+void MJPFileComparator::printMismatchingAmounts() const
+{
+
+	for (auto& entry : customFile.getAllEntries())
+	{
+		if (foxBeleidFile.containsKey(entry.getKey()))
+		{
+			auto& customEntry = entry;
+			auto& foxbeleidEntry = foxBeleidFile.getEntry(entry.getKey());
+
+			if (customEntry.getAmounts() != foxbeleidEntry.getAmounts())
+			{
+				std::cerr << "Amounts did not match for key " << entry.getKey() << "\n";
+				std::cerr << "\tCustom file: " << customEntry.getAmounts() << "\n";
+				std::cerr << "\tFox beleid:  " << foxbeleidEntry.getAmounts() << "\n";
+			}
+		}
+	}
+}
--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,53 +1,23 @@
-#include "MJPFile.hpp"
-
-#include <boost/filesystem.hpp>
+#include "MJPFileComparator.h"
 
 using boost::filesystem::path;
 
-namespace std {
-
-std::ostream& operator<<(std::ostream& ws, const std::vector<double>& amounts)
-{
-	std::copy(amounts.begin(), amounts.end(), std::ostream_iterator<double>(ws, " "));
-	return ws;
-}
-
-}  // namespace std
-
 int main(int argc, const char** argv)
 {
-	MJPFile foxBeleidMJPFile{path(DATA_DIR)/ "20141117_2230_MJP_foxbeleid.csv", convertFoxBeleidMJPLine};
-	MJPFile customMJPFile   {path(DATA_DIR)/ "20141117_2230_MJP_custom.csv",    convertCustomMJPLine};
+	path foxBeleidMJPFile{path(DATA_DIR)/ "20141120_0330_MJP_foxbeleid.csv"};
+	path customMJPFile   {path(DATA_DIR)/ "20141120_0330_MJP_custom.csv"};
+	MJPFileComparator comparator(foxBeleidMJPFile, customMJPFile);
 
-	for (auto& entry : customMJPFile.getAllEntries())
+	for (auto& entry : comparator.getEntriesMissingInFoxBeleid())
 	{
-		if (!foxBeleidMJPFile.containsKey(entry.getKey()))
-		{
-			std::cerr << "Could not find following key in foxBeleid mjp file: " << entry.getKey() << "\n";
-		}
+		std::cerr << "Could not find following key in foxBeleid mjp file: " << entry.getKey() << "\n";
 	}
 
-	for (auto& entry : foxBeleidMJPFile.getAllEntries())
+	for (auto& entry : comparator.getEntriesMissingInCustomFile())
 	{
-		if (!customMJPFile.containsKey(entry.getKey()))
-		{
-			std::cerr << "Could not find following key in custom mjp file: " << entry.getKey() << "\n";
-		}
+		std::cerr << "Could not find following key in custom mjp file: " << entry.getKey() << "\n";
 	}
 
-	for (auto& entry : customMJPFile.getAllEntries())
-	{
-		if (foxBeleidMJPFile.containsKey(entry.getKey()))
-		{
-			auto& customEntry = entry;
-			auto& foxbeleidEntry = foxBeleidMJPFile.getEntry(entry.getKey());
-
-			if (customEntry.getAmounts() != foxbeleidEntry.getAmounts())
-			{
-				std::cerr << "Amounts did not match for key " << entry.getKey() << "\n";
-				std::cerr << "\tCustom file: " << customEntry.getAmounts() << "\n";
-				std::cerr << "\tFox beleid:  " << foxbeleidEntry.getAmounts() << "\n";
-			}
-		}
-	}
+	comparator.printMismatchingAmounts();
+	std::cout << "Thank you, come again!" << std::endl;
 }
